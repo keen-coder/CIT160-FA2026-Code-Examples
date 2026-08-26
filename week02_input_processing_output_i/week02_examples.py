3fa20dd783dbc1b43d15e28d8136d59ea8dfb2a3
keen-coder <kknaur@gmail.com>
2026-08-26 12:44:40 -0400

    Added Week 02 Examples

---
+++ week02_input_processing_output_i/week02_examples.py
@@ -0,0 +1,73 @@
+def main(): 
+    x = 10
+   
+    # print the name keenan
+    print('keenan')
+    
+    print('C:\\Users\\kpk6\\OneDrive - Pennsylvania College of Technology\\CIT160 - Programming I\\cit160_workspace_fa2026\\week02_input_processing_output_i')
+    
+    name = 'keenan'
+    x = 10
+    pi = 3.14159
+    
+    print('name:\t', name)
+    print('x:\t', x)
+    
+    print('hello\nworld')
+    
+    x = 'blue'
+    
+    print('I can\'t go to work today')
+    print("His name is \"Bob\" from that movie")
+    
+    print("hello, world")
+    print(2)
+    print(2 + 3)
+    print('2 + 3 = ', (2 + 3))
+    print('blue', 5, 'red', 13)
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # x, y, z = 10, 3.14, 'blue'
+
+    # x = 10
+    # y = 3.14
+    # z = 'blue'
+    
+
+
+    # print('Name:\tKeenan') # print a name label follow
+    # print('Color:\tGreen')
+    
+    # print("""C:\Users\kpk6\OneDrive - Pennsylvania College of Technology\CIT160 - Programming I\cit160_workspace_fa2026\week02_input_processing_output_i""")
+    
+    # x = 'red'
+    
+    # print('His name is \'Bob\' from that movie')
+    # print('His name is "Bob" from that movie')
+    # print('2 + 2 = ' + str(2 + 2))
+    # print('2' + '2')
+    # print('hello, world')
