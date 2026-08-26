740ca39cfe75f1d2b03aa52e63f01a03f173633b
keen-coder <kknaur@gmail.com>
2026-08-26 12:50:15 -0400

    Updated Week 02 examples

--- week02_input_processing_output_i/variable_demo2.py
+++ week02_input_processing_output_i/variable_demo2.py
@@ -1,7 +1,12 @@
-# Create two variables: top_speed and distance.
-top_speed = 160
-distance = 300
+def main():
 
-# Display the values referenced by the variables.
-print('The top speed is', top_speed)
-print('The distance traveled is',distance)
+    # Create two variables: top_speed and distance.
+    top_speed = 160
+    distance = 300
+
+    # Display the values referenced by the variables.
+    print('The top speed is', top_speed)
+    print('The distance traveled is',distance)
+
+if __name__ == '__main__':
+    main()
